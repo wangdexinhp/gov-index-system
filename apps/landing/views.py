462e3a55bde74176ada8aa0f5ec03357900e5a84
wangdexin <wangdexin@baihai.ai>
2026-04-29 11:30:08 +0800

    order test

--- apps/landing/views.py
+++ apps/landing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 from django.conf import settings
+from apps.coredata.management.commands.import_china_regions import CHINA_REGIONS
 
 @require_http_methods(['GET'])
 def home(request):
@@ -52,6 +54,69 @@ def pricing(request):
 def features(request):
     return render(request, 'landing/features.html') 
 
-# @require_http_methods(['GET'])
-# def features(request):
-#     return render(request, 'landing/indic_order.html') 
+
+# ==================== 购买页面城市数据 API ====================
+
+@require_http_methods(['GET'])
+def municipality_city(request):
+    """
+    获取直辖市列表
+    返回格式: [
+        {"name": "北京市", "code": "110100"},
+        {"name": "天津市", "code": "120100"},
+        {"name": "上海市", "code": "310100"},
+        {"name": "重庆市", "code": "500100"},
+    ]
+    """
+    try:
+        municipalities = []
+        for province in CHINA_REGIONS:
+            # 直辖市：北京市、天津市、上海市、重庆市
+            if province['province_name'] in ('北京市', '天津市', '上海市', '重庆市'):
+                for city in province.get('cities', []):
+                    municipalities.append({
+                        'name': city['name'],
+                        'code': city['code'],
+                    })
+        return JsonResponse({
+            'success': True,
+            'data': municipalities
+        })
+    except Exception as e:
+        return JsonResponse({
+            'success': False,
+            'message': str(e),
+            'data': []
+        })
+
+
+@require_http_methods(['GET'])
+def not_municipality_city(request):
+    """
+    获取非直辖市的城市列表（普通地级市）
+    返回格式: [
+        {"name": "石家庄市", "code": "130100"},
+        {"name": "唐山市", "code": "130200"},
+        ...
+    ]
+    """
+    try:
+        cities = []
+        municipality_names = ('北京市', '天津市', '上海市', '重庆市')
+        for province in CHINA_REGIONS:
+            if province['province_name'] not in municipality_names:
+                for city in province.get('cities', []):
+                    cities.append({
+                        'name': city['name'],
+                        'code': city['code'],
+                    })
+        return JsonResponse({
+            'success': True,
+            'data': cities
+        })
+    except Exception as e:
+        return JsonResponse({
+            'success': False,
+            'message': str(e),
+            'data': []
+        })
